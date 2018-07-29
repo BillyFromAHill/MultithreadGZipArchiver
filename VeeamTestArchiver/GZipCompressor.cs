f2218e8b2397dab19f87aebd47b8d3e5309244ce
div <div@mtl.ru>
2018-07-29 18:58:31 +0300

    Инициирован репозиторий.

---
+++ VeeamTestArchiver/GZipCompressor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VeeamTestArchiver
+{
+    public class GZipCompressor
+    {
+        private uint _blockSizeBytes;
+
+        private string _sourceFileName;
+
+        public GZipCompressor(string filename, uint blockSize = 1024 * 1024)
+        {
+            if (string.IsNullOrEmpty(filename))
+            {
+                throw new ArgumentNullException("fileName");
+            }
+
+            _sourceFileName = filename;
+            _blockSizeBytes = blockSize;
+        }
+
+        public void Compress(string destinationPath)
+        {
+
+        }
+
+        public void Decompress(string destinationPath)
+        {
+
+        }
+    }
+}
